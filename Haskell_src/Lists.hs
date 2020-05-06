6c8c536f16adf3a389820c13076763f0eaa08abc
TeraByte1 <teraesver@gmail.com>
2020-05-06 15:19:42 -0700

    cleaning up lists used for testing in Haskell

--- Haskell_src/Lists.hs
+++ Haskell_src/Lists.hs
@@ -1,22 +1,17 @@
 import Data.List
 import System.IO 
---import Criterion.Main
---import Criterion.Main
---use :set +s 
 
---each list comprehension returns a resulting list 
+--resulting list = [0, 9, 36]
+exampleList = [ x * x | x <-[0..6], x `mod` 3 == 0] 
 
---timeIt :: IO a -> IO a
---(1) example our slides: returns squares from range 0-6 divisible by 3
-exampleList = [ x * x | x <-[0..6], x `mod` 3 == 0] --[0,9,36]
 biggerExampleList = [x * x | x <- [0..10000], x `mod` 3==0]
+
 hugeExampleList = [x * x | x <- [0..100000], x `mod` 3==0]
 
-
---(2) tested with inputList [10..20] - if x is less than 15, print BOOP else print BEEP
+--tested with inputList [10..20] - if x is less than 15, print BOOP else print BEEP
 boops inputList  = [ if x < 15 then "BOOP" else "BEEP" | x <- inputList]
 
---(3) sort a list 
+--sort a list 
 sortedList = sort [4, 39, 18, 27, 13, 15, 200, 40001, 3, 2, 2938, 100, 101]
 
 --remove vowels from word
@@ -26,5 +21,3 @@ rmVowels = [x | x <- "supercalifragilisticexpialidocious", not (elem x "aeio")]
 --inner list comprehension: x multiplied by every value in y
 --outer list comprehension: perform inner list comprehension operations for values in x: 1-3 
 multTable = [ [x * y | y <- [1..3]] | x <-[1..3] ]
-
-maxList = [1, 7, 13, 200, 15, 100]
